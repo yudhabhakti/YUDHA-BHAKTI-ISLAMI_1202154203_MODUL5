bcd680b87e822e8f2a643fbd985477b5f6a62c68
ASUS <yudhabhakti1996@gmail.com>
2018-03-25 22:23:23 +0700

    push pertama kali bro

---
+++ app/src/main/java/com/example/asus/yudhabhaktiislami_1202154203_studycase5/AddData.java
@@ -0,0 +1,39 @@
+package com.example.asus.yudhabhaktiislami_1202154203_studycase5;
+
+
+public class AddData {
+    //deklarasi variable
+    String todo, desc, prior;
+
+    //konstruktor
+    public AddData(String todo, String desc, String prior){
+        this.todo=todo;
+        this.desc=desc;
+        this.prior=prior;
+    }
+
+    //method setter dan getter untuk to do , description dan priority
+    public String getTodo() {
+        return todo;
+    }
+
+    public void setTodo(String todo) {
+        this.todo = todo;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getPrior() {
+        return prior;
+    }
+
+    public void setPrior(String prior) {
+        this.prior = prior;
+    }
+}
---
+++ app/src/main/java/com/example/asus/yudhabhaktiislami_1202154203_studycase5/Database.java
@@ -0,0 +1,71 @@
+package com.example.asus.yudhabhaktiislami_1202154203_studycase5;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+
+public class Database extends SQLiteOpenHelper {
+    //deklarasi variable yang akan digunakan
+    Context cntx;
+    SQLiteDatabase db;
+
+    public static final String nama_db = "listtodo.db";
+    public static final String nama_tabel = "daftartodo";
+    public static final String kolom_1 = "todo";
+    public static final String kolom_2 = "description";
+    public static final String kolom_3 = "priority";
+
+    //kontruktor
+    public Database(Context context) {
+        super(context, nama_db, null, 1);
+        this.cntx = context;
+        db = this.getWritableDatabase();
+        db.execSQL("create table if not exists "+nama_tabel+" (todo varchar(35) primary key, description varchar(50), priority varchar(3))");
+    }
+
+    //ketika database dibuat
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL("create table if not exists "+nama_tabel+" (todo varchar(35) primary key, description varchar(50), priority varchar(3))");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL("drop table if exists "+nama_tabel);
+        onCreate(sqLiteDatabase);
+    }
+
+    public boolean inputdata(AddData list) {
+        //mencocokkan kolom beserta nilainya
+        ContentValues val = new ContentValues();
+        val.put(kolom_1, list.getTodo());
+        val.put(kolom_2, list.getDesc());
+        val.put(kolom_3, list.getPrior());
+        long hasil = db.insert(nama_tabel, null, val);
+        if (hasil==-1) {
+            return false;
+        }else {
+            return true;
+        }
+    }
+
+    //method untuk menghapus data pada database
+    public boolean removedata(String ToDo) {
+        return db.delete(nama_tabel, kolom_1+"=\""+ToDo+"\"", null)>0;
+    }
+
+    //method untuk mengakses dan membaca data dari database
+    public void readdata(ArrayList<AddData> daftar){
+        Cursor cursor = this.getReadableDatabase().rawQuery("select todo, description, priority from "+nama_tabel, null);
+        while (cursor.moveToNext()){
+            daftar.add(new AddData(cursor.getString(0), cursor.getString(1), cursor.getString(2)));
+        }
+    }
+}
